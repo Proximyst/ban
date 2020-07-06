e750bcd2e3b82efe681ea613226b9791c5f784cc
Mariell Hoversholm <proximyst@proximyst.com>
2020-07-06 14:22:44 +0200

    Use a tinyint for key in version

--- src/main/resources/sql/create-version-table.sql
+++ src/main/resources/sql/create-version-table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `meta_version`
 (
+    `key` TINYINT(1) NOT NULL DEFAULT 0,
     `version` INT NOT NULL,
 
-    PRIMARY KEY (`version`)
+    PRIMARY KEY (`key`)
 );
 INSERT IGNORE INTO `meta_version` (`version`) VALUES (0);
